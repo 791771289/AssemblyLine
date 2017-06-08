fc52f86d08f75281284fe41bcc79d39578fb7ace
AveryDog <swankerzzzz@gmail.com>
2017-06-08 10:27:12 -0500

    Basic Hands
    Added Controller Hands and objects to manipulate with them.

---
+++ Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class GrabbableObject : MonoBehaviour {
+
+    // The TYPE of pickup the object wiill take on
+    // If the object is FREE - the object has no angle rotations on pickup
+    // If the object is LOCK - the object has angle rotations on pickup
+    public enum TYPE { FREE, LOCK }
+    public TYPE curentType = TYPE.FREE;
+
+    // The LOCK rotation for manipulating grabable objects, when you pick up an object,
+    // it snaps to a different rotation besides remaining idle
+    public Vector3 lockRotation = Vector3.zero;
+   
+}
