17254cb709b3ee4a56db389f717e32ceb80eec68
AveryDog <swankerzzzz@gmail.com>
2017-06-12 11:50:15 -0500

    Fixed too much bouncy and modified candy spawner

--- Assets/Scripts/AssemblyLineStart.cs
+++ Assets/Scripts/AssemblyLineStart.cs
@@ -5,13 +5,33 @@ public class AssemblyLineStart : MonoBehaviour
 {
     public GameObject[] candies;
 
+    private float offsetRange = .3f;
+     
     void Start()
     {
-        foreach(GameObject obj in candies)
+        StartCoroutine(SpawnCandy(.3f, .15f));
+    }
+
+    IEnumerator SpawnCandy(float refreshRate, float timeBetweenSpawns)
+    {
+        for(int i =0; i < 100; i++)
         {
-            float offset = Random.Range(-.5f, .5f);
-            Instantiate(obj, transform.position + new Vector3(0,0,offset), Quaternion.identity);
+            foreach (GameObject obj in candies)
+            {
+                float offset = Random.Range(-offsetRange, offsetRange);
+                float quatX = Random.Range(0, 360f);
+                float quatY = Random.Range(0, 360f);
+                float quatZ = Random.Range(0, 360f);
+                float quatW = Random.Range(0, 360f);
+
+                Instantiate(obj, transform.position + new Vector3(0, 0, offset), new Quaternion(quatX, quatY, quatZ, quatW));
+                yield return new WaitForSeconds(timeBetweenSpawns);
+            }
+
+            yield return new WaitForSeconds(1f / refreshRate);
         }
+
+        yield break;
     }
 
 }
