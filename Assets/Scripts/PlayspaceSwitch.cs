f64fe0a8d3807f937fe950809b25698956ac6753
AveryDog <swankerzzzz@gmail.com>
2017-06-19 00:23:02 -0500

    Added Transform switch and small changes to Pointer

---
+++ Assets/Scripts/PlayspaceSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayspaceSwitch : MonoBehaviour {
+
+    // Types of transfrom changes
+    // UP - closer to the assembly line
+    // DOWN - further away
+	public enum TYPES { UP, DOWN}
+    public TYPES currenttType;
+
+    // Playspace transform to manipulate
+    public Transform playSpace;
+
+    // Step for how much the transform moves everytime the switch is pressesd
+    private float stepOffeset = 1f;
+
+    public void UpdateTransform()
+    {
+        if(currenttType == TYPES.UP)
+        {
+            playSpace.position += new Vector3(0, 0, stepOffeset);
+        } else
+        {
+            playSpace.position += new Vector3(0, 0, -stepOffeset);
+        }
+    }
+}
---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class Pointer : MonoBehaviour {
+
+    // Line Attributes for the pointer
+    [Header("LineRenderer Attributes")]
+    public float lineWidth;
+    public float lineLength;
+    public Material lineMaterial;
+
+    // Set objects that control the lines position
+    [Header("LineRenderer Setup")]
+    public Transform startPoint;
+    public Transform endPoint;
+    private Vector3 endPointScale;
+
+    // Instance of linerenderer
+    private LineRenderer lineRenderer;
+
+    // Pulling the SteamVR controller from its index
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    void Start()
+    {
+        // Grabbing components from the game object
+        lineRenderer = GetComponent<LineRenderer>();
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        endPoint.GetComponent<MeshRenderer>().material = lineMaterial;
+        endPointScale = endPoint.localScale;
+
+        // Update pointer's attributes
+        lineRenderer.SetWidth(lineWidth, lineWidth);
+        lineRenderer.material = lineMaterial;
+    }
+
+    void Update()
+    {
+        // Send out a ray to detect where the pointer should be pointing
+        Ray ray = new Ray(startPoint.position, startPoint.forward);
+        RaycastHit hit;
+        bool hitting = Physics.Raycast(ray, out hit, lineLength);
+
+        if(hit.transform.tag == "UI")
+        {
+            UpdateCursorLine(hitting, hit);
+        } else
+        {
+            // Line disabled
+            endPoint.localPosition = Vector3.zero;
+            endPoint.transform.localScale = Vector3.zero;
+        }
+    }
+
+    void UpdateCursorLine(bool hitting, RaycastHit hit)
+    {
+
+        if (hitting)
+        {
+            // Line Enabled
+            endPoint.position = hit.point;
+        }
+        else
+        {
+            // Line disabled
+            endPoint.localPosition = Vector3.zero;
+            endPoint.transform.localScale = Vector3.zero;
+        }
+
+        // Constantly updated set points on the line
+        lineRenderer.SetPosition(0, startPoint.position);
+        lineRenderer.SetPosition(1, endPoint.position);
+
+    }
+}
